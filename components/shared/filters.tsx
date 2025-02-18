672d767eec6f9b9c116b7308399ba7941db24756
ALEXsiy <dima-bat2003@mail.ru>
2025-02-18 23:50:28 +0300

    подключил библиотеку types/qs для преобразования параметров в строку в url

--- components/shared/filters.tsx
+++ components/shared/filters.tsx
@@ -4,23 +4,36 @@ import { Title } from './title';
 import { Input } from '../ui';
 import { RangeSlider } from './range-slider';
 import { CheckboxFiltersGroup } from './checkbox-filters-group';
-import { useFilterIngredients } from '@/hooks/useFilterIngredients';
+import { useFilterIngredients } from '@/hooks/use-filter-Ingredients';
 import { useSet } from 'react-use';
+import qs from 'qs'
+import { useRouter, useSearchParams } from 'next/navigation';
 
 interface Props {
     className?: string;
 }
 
 interface PriceProps{
-    priceFrom: number;
-    priceTo: number;
+    priceFrom?: number;
+    priceTo?: number;
+}
+
+interface QueryFilters extends PriceProps{
+    pizzaTypes: string;
+    sizes: string;
+    ingredients: string;
 }
 
 export const Filters: React.FC<Props> = ({ className}) => {
-    const {ingredients, loading, onAddId, selectedIngredients} = useFilterIngredients();
-    const [sizes, {toggle: toggleSizes }] = useSet(new Set<string>([]));
-    const [pizzaTypes, {toggle: togglePizzaTypes }] = useSet(new Set<string>([]));
-    const [prices, setPrice] = React.useState<PriceProps>({priceFrom: 0, priceTo: 1000});
+    const searchParams = useSearchParams() as unknown as Map<keyof QueryFilters, string>;
+    const router = useRouter();
+    const {ingredients, loading, onAddId, selectedIngredients} = useFilterIngredients(searchParams.get('ingredients')?.split(','),);
+    const [sizes, {toggle: toggleSizes }] = useSet(new Set<string>(searchParams.has('sizes') ? searchParams.get('sizes')?.split(',') : []));
+    const [pizzaTypes, {toggle: togglePizzaTypes }] = useSet(new Set<string>((searchParams.has('pizzaTypes') ? searchParams.get('pizzaTypes')?.split(',') : [])));
+    const [prices, setPrice] = React.useState<PriceProps>({
+        priceFrom: Number(searchParams.get('priceFrom')) || undefined,
+        priceTo: Number(searchParams.get('priceTo')) || undefined,
+    });
 
 
     const items = ingredients.map((item) => ({value: String(item.id), text: item.name}));
@@ -34,8 +47,16 @@ export const Filters: React.FC<Props> = ({ className}) => {
     };
 
     React.useEffect(() =>{
-        console.log({prices, pizzaTypes, sizes, selectedIngredients});
-    }, [prices, pizzaTypes, sizes, selectedIngredients]);
+        const filters ={
+            ...prices,
+            pizzaTypes: Array.from(pizzaTypes),
+            sizes: Array.from(sizes),
+            ingredients: Array.from(selectedIngredients)
+        };
+        const query = qs.stringify(filters, {arrayFormat: 'comma'});
+
+        router.push(`?${query}`, {scroll: false});
+    }, [prices, pizzaTypes, sizes, selectedIngredients, router]);
 
     return (
         <div className={className}>
@@ -77,8 +98,8 @@ export const Filters: React.FC<Props> = ({ className}) => {
                     onChange={(e) => updatePrice('priceTo', Number(e.target.value))}/>
                 </div>
                 <RangeSlider min={0} max={1000} step={10} value={[
-                    prices.priceFrom,
-                    prices.priceTo
+                    prices.priceFrom || 0,
+                    prices.priceTo || 1000
                 ]} 
                 onValueChange={([priceFrom, priceTo]) => setPrice({priceFrom: priceFrom, priceTo: priceTo})}></RangeSlider>
             </div>
--- hooks/use-filter-Ingredients.ts
+++ hooks/use-filter-Ingredients.ts
@@ -12,11 +12,11 @@ interface ReturnProps {
     onAddId: (id: string) => void;
 }
 
-export const useFilterIngredients = (): ReturnProps => {
+export const useFilterIngredients = (values: string[] = []): ReturnProps => {
     const [ingredients, setIngredients] = React.useState<Ingredient[]>([]);
     const [loading, setLoading] = React.useState(true);
 
-    const [selectedIds, {toggle}] = useSet(new Set<string>([]));
+    const [selectedIds, {toggle}] = useSet(new Set<string>(values));
 
     React.useEffect(() =>{
         async function fetchIngredients() {
@@ -34,5 +34,10 @@ export const useFilterIngredients = (): ReturnProps => {
         fetchIngredients();
 
     }, []);
-    return {ingredients, loading, onAddId: toggle, selectedIngredients: selectedIds };
+
+    const setSelectedIngredients = (ids: string[]) => {
+        ids.forEach(selectedIds.add);
+    }
+
+    return {ingredients, loading, onAddId: toggle, selectedIngredients: selectedIds, };
 }
